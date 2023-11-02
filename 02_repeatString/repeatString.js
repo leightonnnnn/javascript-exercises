741fd3e3d032cea0625eb29b33b163fc2b8a854b
Leighton Anderson <leightonwanderson@gmail.com>
2023-11-02 16:00:51 -0500

    finish code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(repeatSentence, numTimes) {
+    let i;
+    let strSpace = "";
+    const randomTimes = Math.floor(Math.random());
+    for (i = 0; i < numTimes; i++){
+        if (numTimes < 0) {
+            break;
+        }
+        strSpace+=repeatSentence;
+    }
+    if (numTimes < 0) {
+        return "ERROR";
+    } else {
+    return strSpace;
+}
 };
 
 // Do not edit below this line
